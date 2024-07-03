076f47c44b86d90f50262de40ecc29d3cba88613
Huge <huge@agi7.ai>
2024-07-03 14:52:54 +0800

    fix DoFindH265PayloadType

--- video/rtp_video_stream_receiver2.cc
+++ video/rtp_video_stream_receiver2.cc
@@ -1434,7 +1434,7 @@ void RtpVideoStreamReceiver2::UpdatePacketReceiveTimestamps(
 
 int RtpVideoStreamReceiver2::DoFindH265PayloadType() {
   int payload_type = 0;
-  for (const Decoder& decoder : decoders) {
+  for (const VideoReceiveStreamInterface::Decoder& decoder : decoders) {
     RTC_LOG(LS_INFO) << "decoder.video_format.name=" << decoder.video_format.name;
     if (decoder.video_format.name == "H265") {
       payload_type = decoder.payload_type;
