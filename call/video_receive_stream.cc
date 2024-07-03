eded91dff08d3211c04c51eb4afd3d072806a198
Huge <huge@agi7.ai>
2024-07-03 14:29:13 +0800

    add FindH265PayloadType

--- call/video_receive_stream.cc
+++ call/video_receive_stream.cc
@@ -165,4 +165,27 @@ std::string VideoReceiveStreamInterface::Config::Rtp::ToString() const {
   return ss.str();
 }
 
+int VideoReceiveStreamInterface::Config::DoFindH265PayloadType() {
+  int payload_type = 0;
+  for (const Decoder& decoder : config_.decoders) {
+    RTC_LOG(LS_INFO) << "decoder.video_format.name=" << decoder.video_format.name;
+    if (decoder.video_format.name == "H265") {
+      payload_type = decoder.payload_type;
+      RTC_LOG(LS_INFO) << "DoFindH265PayloadType: Found H265 payload type=" << payload_type;
+      break;
+    }
+  }
+  if (payload_type == 0) {
+    RTC_LOG(LS_WARNING) << "DoFindH265PayloadType: Cannot find H265 payload type";
+  }
+  return payload_type;
+}
+
+int VideoReceiveStreamInterface::Config::FindH265PayloadType() {
+  if (h265_payload_type == 0) {
+    h265_payload_type = DoFindH265PayloadType();
+  }
+  return h265_payload_type;
+}
+
 }  // namespace webrtc
